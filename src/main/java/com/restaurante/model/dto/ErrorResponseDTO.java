cf5e82df37ea3e98bc58497c8ed6895923356675
kevin Angel <kevin.angel-a@mail.escuelaing.edu.co>
2026-09-21 20:44:18 -0500

    feat: agregar ErrorResponseDTO para respuestas de error uniformes

---
+++ src/main/java/com/restaurante/model/dto/ErrorResponseDTO.java
@@ -0,0 +1,21 @@
+package com.restaurante.model.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponseDTO {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+}
