5a5650d70767cc182c4adf924417cc019f2e2820
kevin Angel <kevin.angel-a@mail.escuelaing.edu.co>
2026-09-21 21:19:50 -0500

    fix: manejar HttpMessageNotReadableException (body vacio/invalido) como 400 en vez de 500

--- src/main/java/com/restaurante/controller/GlobalExceptionHandler.java
+++ src/main/java/com/restaurante/controller/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -58,6 +59,13 @@ public class GlobalExceptionHandler {
                 .collect(Collectors.joining(", "));
         log.warn("Validacion de input fallida: {}", mensaje);
         return construirRespuesta(HttpStatus.BAD_REQUEST, mensaje, request);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseDTO> manejarBodyInvalido(HttpMessageNotReadableException ex,
+                                                                  HttpServletRequest request) {
+        log.warn("Body invalido o vacio en la peticion: {}", request.getRequestURI());
+        return construirRespuesta(HttpStatus.BAD_REQUEST, "El cuerpo de la peticion es invalido o esta vacio", request);
     }
 
     @ExceptionHandler(Exception.class)
